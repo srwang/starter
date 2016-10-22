0889c58232e905ce9d6e3009702fdc56de0ac2c5
Sharon Wag <sharon.ran.wang@gmail.com>
2016-10-22 16:34:40 -0400

    close modal js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('linked')
+$(document).ready(function(){
+
+	//close on close button
+	$('.stay-wanderful-iframe').contents().on('click', '.close-modal-button', function() {
+		$('.stay-wanderful-iframe').hide();
+	})
+	//close on overlay
+	$('.stay-wanderful-iframe').contents().on('click', 'html', function() {
+		$('.stay-wanderful-iframe').hide();
+	})
+
+});
